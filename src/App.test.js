97044dcd3aed2cbf1e5aad45f2042a03e9715d1e
Oriol Grau <ograup@gmail.com>
2020-12-17 21:31:11 +0100

    Basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders the form', () => {
+    render(<App />);
+
+    const firstNumberLabel = screen.getByLabelText(/first number:/i);
+    const secondNumberLabel = screen.getByLabelText(/second number:/i);
+
+    expect(firstNumberLabel).toBeInTheDocument();
+    expect(secondNumberLabel).toBeInTheDocument();
+  });
 });
