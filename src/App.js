12f61ab161bad9ec11bff0913c2c0fd8ea48e8e7
Oriol Grau <ograup@gmail.com>
2020-12-17 21:17:01 +0100

    Markup and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [numbers, setNumber] = useState({ first: '', second: '' });
+
+  function handleChange(e) {
+    const { value, name } = e.target;
+    setNumber(numbers => ({ ...numbers, [name]: value }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <header className="app-header">ITSAPARK Challenge</header>
+      <div className="form-wrapper">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="first">First number:</label>
+          <input
+            type="number"
+            name="first"
+            id="first"
+            required
+            value={numbers.first}
+            onChange={handleChange}
+          />
+          <label htmlFor="second">Second number:</label>
+          <input
+            type="number"
+            name="second"
+            id="second"
+            required
+            value={numbers.second}
+            onChange={handleChange}
+          />
+          <input type="submit" value="PROCESS" />
+        </form>
+      </div>
     </div>
   );
 }
